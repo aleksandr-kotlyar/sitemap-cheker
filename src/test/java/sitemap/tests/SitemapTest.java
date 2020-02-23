dc02c3c10b097296dcb68380c2b96ead437b7ff5
Aleksandr Kotlyar <ask.kotlyar@gmail.com>
2020-02-23 23:28:43 +0300

    Pass variable to test from property and throw handle message if empty (Resolves 11)

--- src/test/java/sitemap/tests/SitemapTest.java
+++ src/test/java/sitemap/tests/SitemapTest.java
@@ -1,14 +1,17 @@
 package sitemap.tests;
 
-import sitemap.StepsLogger;
 import io.restassured.response.Response;
 import org.assertj.core.api.SoftAssertions;
 import org.jsoup.Jsoup;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import sitemap.StepsLogger;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 import static io.restassured.RestAssured.given;
 
@@ -16,13 +19,14 @@ import static io.restassured.RestAssured.given;
  * Created by Aleksandr Kotlyar on 09.11.2017. 21:44
  */
 public class SitemapTest extends StepsLogger {
-    private static final String SITEMAP_URL = "https://qiwi.com/sitemap.xml";
     private static String SITEMAP_PAGE_SOURCE;
+    private String SITEMAP_URL;
     private List<String> allSitemapLinks = new ArrayList<>();
     private SoftAssertions softAssertions = new SoftAssertions();
 
     @Before
     public void checkSitemapIsAvailable() throws Exception {
+        SITEMAP_URL = getSitemapUrl();
         given().header("user-agent", "yandex").when().get(SITEMAP_URL).then().statusCode(200);
     }
 
@@ -51,5 +55,18 @@ public class SitemapTest extends StepsLogger {
         Response response = given().when().head(link);
         info(String.format("%2d;%s", response.statusCode(), link));
         softAssertions.assertThat(response.statusCode()).as("link: '" + link + "'").isEqualTo(200);
+    }
+
+    private String getSitemapUrl() {
+        Properties properties = new Properties();
+        try {
+            properties.load(SitemapTest.class.getClassLoader().getResourceAsStream("Sitemap.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+            Assert.fail();
+        }
+        String url = properties.getProperty("sitemapUrl");
+        Assert.assertNotEquals("Target url is empty! Set sitemapUrl in pom.xml or ${SITEMAPURL} in Gitlab-CI", "", url);
+        return url;
     }
 }
